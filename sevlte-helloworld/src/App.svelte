6d5cdfe0cf1c293447eda4942f6a1f6b15f41064
tejas-2552 <Rtejas3730@gmail.com>
2024-08-18 00:12:54 +0530

    Project set up tut added

---
+++ sevlte-helloworld/src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	export let name;
+	let m = 'hello world';
+</script>
+
+<main>
+	<h1>{m}!</h1>
+	<h2>{name}!</h2>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ svelte-tut01/src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+	let name = "Jhon Wick";
+	let dogColor = "black";
+	let color = '';
+	const handleOnClick = () => {
+		if(color !== ''){
+			dogColor = color;
+		} else {
+			alert("Cannot be empty!");
+		}
+	}
+	
+	const handleInput = (e) =>
+	{
+		dogColor = e.target.value;
+	}
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<h2>Dog color : {dogColor}</h2>
+	<button on:click={handleOnClick}>Click to update the dog color</button>
+	<input type="text" bind:value={color}>
+</main>
+
+<style>
+	main { 
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
